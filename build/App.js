a812a583a384d3d0b7919c1e7abdba4bd8cc2a31
K Munz <kijamu@gmail.com>
2021-10-03 22:09:49 -0400

    update 2

--- build/App.js
+++ build/App.js
@@ -1,6 +1,10 @@
 import React from "./_snowpack/pkg/react.js";
 import "./css/style.css.proxy.js";
 class PetTile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.change = this.change.bind(this);
+  }
   render() {
     return /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement("figure", null, /* @__PURE__ */ React.createElement("img", {
       className: "petTile",
@@ -13,6 +17,7 @@ class PetTile extends React.Component {
     }, "Delete"));
   }
   change() {
+    console.log("This in Change: " + this);
     this.props.onclick(this.props.id);
   }
 }
@@ -20,41 +25,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {pets: []};
+    this.addEntry = this.addEntry.bind(this);
+    this.deleteEntry = this.deleteEntry.bind(this);
     this.load();
   }
   load() {
     console.log("LOADING");
     fetch("/get", {method: "get", "no-cors": true}).then((response) => response.json()).then((json) => {
       console.log("GOT: " + JSON.stringify(json));
-      this.setState({pets: json});
-    });
-  }
-  addEntry(e) {
-    e.preventDefault();
-    const name = document.querySelector('input[name="name"]'), link = document.querySelector('input[name="link"]'), type = document.querySelector('select[name="type"]'), json = {name: name.value, link: link.value, type: type.value}, body = JSON.stringify(json);
-    console.log(body);
-    fetch("/create", {
-      method: "POST",
-      "no-cors": true,
-      body
-    }).then(function(response) {
-      return response.json();
-    }).then(function(state) {
-      console.log("POSTED: " + JSON.stringify(state));
-      this.setState({pets: state});
-    });
-  }
-  deleteEntry(clickedId) {
-    console.log("Delete ID: " + clickedId);
-    const body = JSON.stringify({id: clickedId});
-    fetch("/delete", {
-      method: "POST",
-      "no-cors": true,
-      body
-    }).then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      console.log("DELETED: " + JSON.stringify(json));
       this.setState({pets: json});
     });
   }
@@ -77,10 +55,35 @@ class App extends React.Component {
       image: pet.link,
       call: pet.call,
       id: pet._id,
-      onclick: () => {
-        this.deleteEntry;
-      }
+      onclick: this.deleteEntry
     }))));
+  }
+  addEntry(e) {
+    e.preventDefault();
+    const name = document.querySelector('input[name="name"]'), link = document.querySelector('input[name="link"]'), type = document.querySelector('select[name="type"]'), json = {name: name.value, link: link.value, type: type.value}, body = JSON.stringify(json);
+    console.log("Add Body: " + body);
+    fetch("/create", {
+      method: "POST",
+      body
+    }).then(function(response) {
+      return response.json();
+    }).then(function(arg) {
+      console.log("This in AddEntry: " + this);
+      this.setState({pets: arg});
+    });
+  }
+  deleteEntry(clickedId) {
+    const body = JSON.stringify({id: clickedId});
+    console.log("Delete Body: " + body);
+    fetch("/delete", {
+      method: "POST",
+      body
+    }).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      console.log("This in Delete: " + this);
+      this.setState({pets: json});
+    });
   }
 }
 export default App;
